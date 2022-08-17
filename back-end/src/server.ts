eae8d1793a0d2e43a782ca46d5ca47f948d217c8
Carla A. C. Ribeiro <carlacarlaclementino@gmail.com>
2022-08-17 09:56:57 -0300

    back-end com o docker configurado

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Server is up and running on port 5000`);
+  console.log(`Server is up and running on port 4000`);
 })
